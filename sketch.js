10cc3deadc65c3ca1ef04b44db591278508d1178
Harold <hhausman@gmail.com>
2022-02-25 17:18:41 -0700

    `[squares]` More bleed for wider screens

--- sketch.js
+++ sketch.js
@@ -5,18 +5,18 @@ function setup() {
 function draw() {
   clear()
   noStroke()
-  background(32)
-  fill(248)
+  background("#222")
+  fill("#f8f8f8")
   let [w,h] = [windowWidth, windowHeight]
   let r = (h>w?w:h) / 40
-  let n = (h>w?w:h) / r / 2
+  let n = ((h>w?w:h) / (r*1.125))|0
   let t = (millis()/1500.0)%4
   let [xt,yt] = {"0": [1,0], "1": [0,1], "2": [-1,0], "3": [0,-1]}[t|0]
   translate(w/2, h/2)
   rotate(PI/4 + millis()/100000)
-  for(let x=-n/2; x<=n/2; x++) {
-    for(let y=-n/2; y<=n/2; y++) {
-      if(sqrt(x*x+y*y)<n/3.5) {
+  for(let x=-(n/3)|0; x<=(n/3)|0; x++) {
+    for(let y=-(n/3)|0; y<=(n/3)|0; y++) {
+      if(sqrt(x*x+y*y)<n/6) {
         circle(2*r*x, 2*r*y, r/2)
       }
     }
