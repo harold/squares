9ade3c96725645b9284d20147db0a38a57eed572
Harold <hhausman@gmail.com>
2022-02-25 17:08:52 -0700

    `[squares]` Init

---
+++ sketch.js
@@ -0,0 +1,39 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+}
+
+function draw() {
+  clear()
+  noStroke()
+  background(32)
+  fill(248)
+  let [w,h] = [windowWidth, windowHeight]
+  let r = (h>w?w:h) / 40
+  let n = (h>w?w:h) / r / 2
+  let t = (millis()/1500.0)%4
+  let [xt,yt] = {"0": [1,0], "1": [0,1], "2": [-1,0], "3": [0,-1]}[t|0]
+  translate(w/2, h/2)
+  rotate(PI/4 + millis()/100000)
+  for(let x=-n/2; x<=n/2; x++) {
+    for(let y=-n/2; y<=n/2; y++) {
+      if(sqrt(x*x+y*y)<n/3.5) {
+        circle(2*r*x, 2*r*y, r/2)
+      }
+    }
+  }
+  translate(4*r*xt*(t%1), 4*r*yt*(t%1))
+  for(let x=-n; x<=n; x++) {
+    for(let y=-n; y<=n; y++) {
+      circle(2*r*x, 2*r*y, r/2)
+    }
+  }
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
+
+if(window.DeviceOrientationEvent) {
+  window.addEventListener("orientationchange", windowResized, false)
+}
